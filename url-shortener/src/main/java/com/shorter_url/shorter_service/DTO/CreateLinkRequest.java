f45db1c0df541588b840e9015735fc37aa665d19
Амир Биктимиров <biktimirov@MacBook-Air-Amir.local>
2026-07-19 19:22:00 -0700

    lvl8: Deploy

--- url-shortener/src/main/java/com/shorter_url/shorter_service/DTO/CreateLinkRequest.java
+++ url-shortener/src/main/java/com/shorter_url/shorter_service/DTO/CreateLinkRequest.java
@@ -1,6 +1,8 @@
 package com.shorter_url.shorter_service.DTO;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.URL;
 
-public record CreateLinkRequest(@NotBlank String originalUrl) {
+public record CreateLinkRequest(@NotBlank @URL @Size(max = 2048) String originalUrl) {
 }
